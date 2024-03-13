166d6708f076cf5dfa90615cfc045f80fa32df0d
amalla4 <amalla4@students.towson.edu>
2024-03-13 01:07:16 -0400

    declaring the implementations as beans using the @Component annotation, removed the config class

--- src/main/java/com/example/demo/services/impl/ColorPrinterImpl.java
+++ src/main/java/com/example/demo/services/impl/ColorPrinterImpl.java
@@ -4,7 +4,9 @@ import com.example.demo.services.BluePrinter;
 import com.example.demo.services.ColorPrinter;
 import com.example.demo.services.GreenPrinter;
 import com.example.demo.services.RedPrinter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ColorPrinterImpl implements ColorPrinter {
     private RedPrinter redPrinter;
     private BluePrinter bluePrinter;
--- src/main/java/com/example/demo/services/impl/EnglishGreenPrinter.java
+++ src/main/java/com/example/demo/services/impl/EnglishGreenPrinter.java
@@ -1,7 +1,9 @@
 package com.example.demo.services.impl;
 
 import com.example.demo.services.GreenPrinter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EnglishGreenPrinter implements GreenPrinter {
     @Override
     public String print(){
